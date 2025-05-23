247a5c215f0eaf8da6cc28091368f3e3243dd533
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-05-23 16:52:00 +0200

    Small improvements in Nix code

--- flake.nix
+++ flake.nix
@@ -3,35 +3,37 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { self, nixpkgs }:
-    let
-      defaultSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      perSystem = nixpkgs.lib.genAttrs defaultSystems;
-    in {
-      packages = perSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          nix2container = import ./. {
-            inherit pkgs system;
-          };
-          examples = import ./examples {
-            inherit pkgs;
-            inherit (nix2container) nix2container;
-          };
-          tests = import ./tests {
-            inherit pkgs examples;
-            inherit (nix2container) nix2container;
-          };
-        in {
-          inherit (nix2container) nix2container-bin skopeo-nix2container nix2container;
-          inherit examples tests;
-          default = nix2container.nix2container-bin;
-        }
-      );
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    defaultSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    perSystem = nixpkgs.lib.genAttrs defaultSystems;
+  in {
+    packages = perSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nix2container = import ./. {
+          inherit pkgs;
+        };
+        examples = import ./examples {
+          inherit pkgs;
+          inherit (nix2container) nix2container;
+        };
+        tests = import ./tests {
+          inherit pkgs examples;
+          inherit (nix2container) nix2container;
+        };
+      in {
+        inherit (nix2container) nix2container-bin skopeo-nix2container nix2container;
+        inherit examples tests;
+        default = nix2container.nix2container-bin;
+      }
+    );
+  };
 }
